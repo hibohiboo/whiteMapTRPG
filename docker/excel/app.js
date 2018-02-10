30b456e3f54060d7e25f293201066cd2164feb24
hibohiboo <swdice66@gmail.com>
2018-02-10 21:37:06 +0900

    コメント修正

--- docker/excel/app.js
+++ docker/excel/app.js
@@ -2,13 +2,15 @@ const fs = require('fs');
 const excel = require('exceljs');
 
 (async _ =>{
+  // コンテナ上のパスを記述
   const src = `/app/data/chart.xlsx`;
   const dest = `/app/dest/chart.pug`;
+
   // エクセルの内容を取得
   const sheets = await getSheets(src);
 
   // 書き出すファイルの内容
-  let content = "";
+  let content = ``;
 
   // シート名を変数名、変数の値をレコードとして、pugファイルの内容を作成
   sheets.forEach(sheet=>{
@@ -17,8 +19,8 @@ const excel = require('exceljs');
 
   // ファイルに書き出し
   fs.writeFile(dest, content, (err)=>console.log(err));
-  console.log(`Excel: ${src} to Pug: ${dest} `);
 
+  console.log(`Excel: ${src} to Pug: ${dest} `);
 })();
 
 /**
@@ -50,18 +52,20 @@ function getSheetData(sheet){
   while(sheet.getCell(1, columIndex).value !== null){
     headerRow.push(sheet.getCell(1, columIndex++).value);
   }
-  columIndex--;
 
+  const columnCount = headerRow.length;
   const dataArr = [];
 
   // 二行目からのデータを取り込み
   rowIndex++;
   while(sheet.getCell(rowIndex, 1).value !== null){
     const data ={};
-    let col=1;
-    while(col<=columIndex){
-      data[headerRow[col-1]] =sheet.getCell(rowIndex, col++).value;
+
+    // ヘッダをプロパティ名として値を設定
+    for(let col = 0; col < columnCount; col++){
+      data[headerRow[col]] =sheet.getCell(rowIndex, col + 1).value;
     }
+
     dataArr.push(data);
     rowIndex++;
   }
